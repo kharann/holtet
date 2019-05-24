8cd34b759602995f37d51c00df7cb6f5a3d564f6
Magnus L. Holtet <magnulho@gmail.com>
2019-05-24 17:33:32 +0200

    Scale image.

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,7 +6,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  border: 1px solid #444;
+`;
+
+const ImageContainer = styled.div`
   overflow: hidden;
+  height: 14rem;
+  width: 14rem;
 `;
 
 const Code = styled.div`
@@ -25,13 +31,16 @@ const Code = styled.div`
   &:hover {
     background-position: left bottom;
     color: whitesmoke;
+    cursor: pointer;
   }
 `;
 
 const Image = styled.img`
   /* Mobile */
-  width: 14rem;
-  height: auto;
+  max-width: 100%;
+  max-height: 100%;
+  min-width: 100%;
+  min-height: 100%;
 
   /* Anything below desktop size*/
   @media screen AND (max-width: 900px) {
@@ -49,21 +58,23 @@ const Image = styled.img`
 
     &:hover {
       filter: blur(0.5rem);
-      transform: scale(1.05);
+      transform: scale(0.95);
       cursor: pointer;
     }
   }
 `;
 
-const handleImageClick = url => {
+const handleOnClick = url => {
   window.open(url, '_blank');
 };
 
-const Project = ({ src, url, code }) => {
+const Project = ({ image, site, code }) => {
   return (
     <Container>
-      <Image src={src} onClick={() => handleImageClick(url)} />
-      <Code>
+      <ImageContainer>
+        <Image src={image} onClick={() => handleOnClick(site)} />
+      </ImageContainer>
+      <Code onClick={() => handleOnClick(code)}>
         <FontAwesomeIcon
           style={{ marginRight: '0.5rem' }}
           icon={faGithub}
