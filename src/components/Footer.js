a9bf542d74b5262abc1c914353a2ed455950d58a
Magnus L. Holtet <magnulho@gmail.com>
2019-05-29 21:50:36 +0200

    Add netlify build status.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,27 @@ import styled from 'styled-components';
 
 const FooterDiv = styled.footer`
   display: flex;
+  flex-direction: column;
+
   justify-content: center;
   align-items: center;
-  height: 3rem;
+  height: 5rem;
   color: #fff;
   margin-top: 1rem;
   background-color: #736f6f;
+
+  & p {
+    padding: 0 0 0.5rem 0;
+  }
 `;
 
 const Footer = () => {
-  return <FooterDiv>&copy; 2019 Magnus Lauritzen Holtet</FooterDiv>;
+  return (
+    <FooterDiv>
+      <p>&copy; 2019 Magnus Lauritzen Holtet </p>
+      <img src='https://api.netlify.com/api/v1/badges/925db648-520d-4c1d-bebb-a001aeb802e9/deploy-status' />
+    </FooterDiv>
+  );
 };
 
 export default Footer;
